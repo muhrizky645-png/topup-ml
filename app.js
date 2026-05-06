e88b300afdb6d014f13fbce0fc60d6e86c7e75a4
muhrizky645-png <muhrizky645@gmail.com>
2026-05-07 04:59:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("TOPUP ML SERVER ACTIVE");
+});
 
 app.get("/products", async (req, res) => {
 
